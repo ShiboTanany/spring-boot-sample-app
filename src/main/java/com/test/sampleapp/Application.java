9f4f0549f97c1c4a7b1809a6764288ddb0630a9f
ShiboTanany <shaaban.altanany@gmail.com>
2019-05-25 15:39:35 +0200

    ajdks

--- src/main/java/com/test/sampleapp/Application.java
+++ src/main/java/com/test/sampleapp/Application.java
@@ -16,7 +16,7 @@ public class Application {
     public static void main(String[] args) {
         final SpringApplication springApplication =
                 new SpringApplication(Application.class);
-        // it is being added here for LOCAL run ONLY , spring profiles should be be run time parameters when run spring boot jar
+        // it is being added here for LOCAL run ONLY , spring profiles should be be run time parameters when run spring boot jarkldskldkslkdlskldsk
         springApplication.setDefaultProperties(Collections.singletonMap("spring.profiles.default","LOCAL"));
         springApplication.addListeners(new ApplicationPidFileWriter());
         springApplication.run(args);
